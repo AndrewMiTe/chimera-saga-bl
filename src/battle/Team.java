eacafc52f8c6f7e2405df4ba8134b4c6e16f4fd5
AndrewMiTe <andrew.m.teller@gmail.com>
2016-08-30 06:25:31 -0400

    Revised comments. Removed email address.

--- src/battle/Team.java
+++ src/battle/Team.java
@@ -10,8 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -26,7 +26,7 @@ package battle;
 
 /**
  * Enumerate the two basic teams a Unit can be part of.
- * @author Andrew M. Teller (andrew.m.teller@gmail.com)
+ * @author Andrew M. Teller(https://github.com/AndrewMiTe)
  */
 public enum Team {
 
