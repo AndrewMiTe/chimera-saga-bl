814eb6bcc455b6a8738a6e6f8e91cfdc539a8698
AndrewMiTe <andrew.m.teller@gmail.com>
2016-08-30 06:22:46 -0400

    Revised comments. Removed email address.

--- src/battle/Row.java
+++ src/battle/Row.java
@@ -10,8 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -27,7 +27,7 @@ package battle;
 /**
  * Enumerates the two rows that a Unit can be located within a given Team, as
  * well as an option for skills that can be used in either Row.
- * @author Andrew M. Teller (andrew.m.teller@gmail.com)
+ * @author Andrew M. Teller(https://github.com/AndrewMiTe)
  */
 public enum Row {
   
