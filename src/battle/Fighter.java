ccde5518ee1c1fe984e8a9ce547330d2e6d127ba
Andrew Michael Teller <andrew.m.teller@gmail.com>
2016-10-08 19:09:05 -0400

    New methods and javadocs added.

--- src/battle/Fighter.java
+++ src/battle/Fighter.java
@@ -24,6 +24,9 @@
 
 package battle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Andrew M. Teller(https://github.com/AndrewMiTe)
@@ -31,44 +34,149 @@ package battle;
 public class Fighter implements Actor {
 
   /**
+   * @see FighterBuilder#setName
+   */
+  private final String name;
+  
+  /**
+   * @see FighterBuilder#setCloseRange
+   */
+  private final int closeRange;
+
+  /**
+   * @see FighterBuilder#addSkill
+   */
+  private final List<Skill> skillList;
+  
+  /**
+   * @see Fighter#applyStatus
+   */
+  private final List<Status> statusList;
+  
+  /**
+   * Initializes the object so that all internal field variables that can be
+   * explicitly set are done so through the given parameters. See the {@link 
+   * FighterBuilder} class which allows you to create Fighter objects using a
+   * builder pattern.
+   * @param name {@see FighterBuilder#setName}
+   * @param closeRange {@see FighterBuilder#setCloseRange}
+   * @param skillList {@see FighterBuilder#addSkill}
+   */
+  public Fighter(String name, int closeRange, List<Skill> skillList) {
+    this.name = name;
+    this.closeRange = closeRange;
+    this.skillList = skillList;
+    this.statusList = new ArrayList<>();
+  }
+  
+  /**
+   * Attempts to apply the given Status object to the fighter. Returns {@code
+   * true} if the predicate for its application returned {@code true}.
+   * @param status status to be applied.
+   * @return {@code true} if the status was applied.
+   */
+  public boolean applyStatus(Status status) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+  
+  /**
    * Attempts to remove the given Status object from the fighter. Returns {@code
    * true} if the object was both found and if the predicate for its removal
    * returned {@code true}. This method will only remove the Status object
    * given.
    * @param status the status to be removed.
-   * @return {@code true} if the object was removed.
+   * @return {@code true} if the status was removed.
    * @see #removeStatus(String name)
-   * @stub
    */
   public boolean removeStatus(Status status) {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
+  /**
+   * Attempts to remove any Status object matching the given name from the
+   * fighter. Returns {@code true} if a match was both found and if the
+   * predicate for its removal returned {@code true}.
+   * @param name name of the status to be removed.
+   * @return {@code true} if the status was removed.
+   */
   public boolean removeStatus(String name) {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
-  public boolean isStunned() {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
-
-  public boolean isDefeated() {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
-
+  /**
+   * Tells the fighter to attempt to execute the given skill. Returns true if
+   * the skill or any sub-skill was successfully executed. The order in which
+   * the given skill is executed is as such:<br><ol>
+   * <li>The battlefield is checked for any valid targets that meet the skill's
+   *    requirements. Valid targets must match the target criteria of the skill.
+   *    The targets must also have all of the matching Status objects found in
+   *    the Skill object's list of requirements. If no valid targets are found,
+   *    then execution fails and this method returns false.</li>
+   * <li>All Skill objects found in the list of sub-skills is executed
+   *    recursively using this method. If all calls return false, then the Skill
+   *    fails and this method returns false. If there are no sub-skills, the
+   *    passed skill succeeds and applies its actions to valid targets. Up to
+   *    the skill's maximum targets property can be affected by the skill,
+   *    starting with those closest to the fighter.</li>
+   * <li>If any but not all sub-skill executions return true, this Skill fails
+   *    to apply its actions but returns true. If all sub-skill executions
+   *    return true, this Skill succeeds and applies its actions to valid
+   *    targets. Up to the skill's maximum targets property can be affected by
+   *    the skill, starting with those closest to the fighter. This method would
+   *    then return true. </li></ol>
+   * Note that valid targets that meet the requirements are checked both before
+   * and after sub-skills are executed. It is possible to create a skill that
+   * can never apply its actions. This will be so if your sub-skills applis a
+   * status that in turn removes a status that the primary skill requires, or if
+   * it applies a status that defeats would-be targets.
+   * @param skill the skill attempting to be executed.
+   * @return {@code true} if the skill or any sub-Skill successfully executed.
+   */
   public boolean executeSkill(Skill skill) {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
+  /**
+   * @return close range property of the fighter.
+   * @see FighterBuilder#setCloseRange
+   */
   public int getCloseRange() {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
-  boolean isAlly(Fighter f) {
+  /**
+   * Returns {@code true} if this fighter is an ally of the given fighter.
+   * @param fighter fighter to test allied relationship with.
+   * @return {@code true} if given fighter is an ally.
+   */
+  boolean isAlly(Fighter fighter) {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
-  boolean isEnemy(Fighter f) {
+  /**
+   * Returns {@code true} if this fighter is an enemy of the given fighter.
+   * @param fighter fighter to test enemy relationship with.
+   * @return {@code true} if given fighter is an enemy.
+   */
+  boolean isEnemy(Fighter fighter) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  /**
+   * Returns {@code true} if the fighter has a status applied to it that stuns
+   * it.
+   * @return {@code true} if stunned.
+   */
+  public boolean isStunned() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  /**
+   * Returns {@code true} if the fighter has a status applied to it that defeats
+   * it.
+   * @return {@code true} if defeated.
+   */
+  public boolean isDefeated() {
     throw new UnsupportedOperationException("Not supported yet.");
   }
 
