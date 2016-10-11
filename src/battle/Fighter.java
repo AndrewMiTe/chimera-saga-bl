1f0d38925267cb35659ca47268281fe877aab35c
Andrew Michael Teller <andrew.m.teller@gmail.com>
2016-10-11 15:31:12 -0400

    Removed executeSkill method

--- src/battle/Fighter.java
+++ src/battle/Fighter.java
@@ -295,39 +295,6 @@ public class Fighter implements Actor {
   }
   
   /**
-   * Tells the fighter to attempt to execute the given skill. Returns true if
-   * the skill or any sub-skill was successfully executed. The order in which
-   * the given skill is executed is as such:<br><ol>
-   * <li>The battlefield is checked for any valid targets that meet the skill's
-   *    requirements. Valid targets must match the target criteria of the skill.
-   *    The targets must also have all of the matching Status objects found in
-   *    the Skill object's list of requirements. If no valid targets are found,
-   *    then execution fails and this method returns false.</li>
-   * <li>All Skill objects found in the list of sub-skills is executed
-   *    recursively using this method. If all calls return false, then the Skill
-   *    fails and this method returns false. If there are no sub-skills, the
-   *    passed skill succeeds and applies its actions to valid targets. Up to
-   *    the skill's maximum targets property can be affected by the skill,
-   *    starting with those closest to the fighter.</li>
-   * <li>If any but not all sub-skill executions return true, this Skill fails
-   *    to apply its actions but returns true. If all sub-skill executions
-   *    return true, this Skill succeeds and applies its actions to valid
-   *    targets. Up to the skill's maximum targets property can be affected by
-   *    the skill, starting with those closest to the fighter. This method would
-   *    then return true. </li></ol>
-   * Note that valid targets that meet the requirements are checked both before
-   * and after sub-skills are executed. It is possible to create a skill that
-   * can never apply its actions. This will be so if your sub-skills applies a
-   * status that in turn removes a status that the primary skill requires, or if
-   * it applies a status that defeats would-be targets.
-   * @param skill the skill attempting to be executed.
-   * @return {@code true} if the skill or any sub-Skill successfully executed.
-   */
-  public boolean executeSkill(Skill skill) {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
-
-  /**
    * @return close range property of the fighter.
    * @see FighterBuilder#setCloseRange
    */
